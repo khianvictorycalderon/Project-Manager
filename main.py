63ae9a1e8b5d26b17d3d4c7177210439a79afda1
khian calderon <ckhianvictory@gmail.com>
2025-06-13 15:03:58 +0800

    Added Back-End + Unique ID Generator Template

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import sys
 # Defaults
 from colors import * # color_1, color_2, up to color_6
 from fonts import * # font_1, font_2, up to font_4
+from global_var import * # db_path
 
 # Templates
+from opxl_temp_ensures import * # ensure_excel_exist, ensure_sheet_exist, and remove_sheet
+from opxl_temp_headers import opxl_write_headers
 from ctk_temp_dual_frame import create_dual_frame
 
 # Pages
@@ -26,6 +29,11 @@ app.state("zoomed")
 app.iconbitmap("./Icons/PJM_Full.ico")
 app.title("Project Manager")
 
+# Ensure that everything works correctly:
+ensure_excel_exist(db_path)
+ensure_sheet_exist(db_path, project_sheet)
+project_sheet_headers = ["ID", "Name", "Description", "Date Started", "Date Finished", "Remarks"]
+opxl_write_headers(db_path, project_sheet, headers=project_sheet_headers)
 
 frame_content = {
     "Project Management": {
