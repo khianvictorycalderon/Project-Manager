d96bdd8b82793883c88abc67c3ee91fa4b02f52d
khian calderon <ckhianvictory@gmail.com>
2025-06-13 13:48:03 +0800

    Added Add Project Page

---
+++ page_add_project.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import customtkinter as ctk
+
+# Defaults
+from colors import * # color_1, color_2, up to color_6
+from fonts import * # font_1, font_2, up to font_4
+
+def add_project_page(parent):
+    ctk.CTkLabel(parent, text="Add Project", font=font_1).pack(pady=20)
