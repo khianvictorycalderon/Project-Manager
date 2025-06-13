f356ed8b3cf0fdc90399765ea9e773b046a1a225
khian calderon <ckhianvictory@gmail.com>
2025-06-13 13:47:49 +0800

    Added Pages + Fixed Import Section Codes

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # Main entry point of the program, run this .py file
 from tkinter import *
 import customtkinter as ctk
+from tkinter import messagebox
+import sys
 
+# Defaults
 from colors import * # color_1, color_2, up to color_6
 from fonts import * # font_1, font_2, up to font_4
+
+# Templates
 from ctk_temp_dual_frame import create_dual_frame
+
+# Pages
+from page_add_project import add_project_page
+from page_edit_project import edit_project_page
+from page_view_delete_project import view_delete_project_page
+from page_credits import credits_page
 
 app = ctk.CTk()
 screen_width = app.winfo_screenwidth()
@@ -15,11 +26,16 @@ app.state("zoomed")
 app.iconbitmap("./Icons/PJM_Full.ico")
 app.title("Project Manager")
 
+
 frame_content = {
     "Project Management": {
-        "Add Project": lambda _: print(),
-        "Edit Project": lambda _: print(),
-        "View and Delete Project": lambda _: print()
+        "Add Project": add_project_page,
+        "Edit Project": edit_project_page,
+        "View and Delete Project": view_delete_project_page,
+    },
+    "Misc": {
+        "Credits": credits_page,
+        "Exit": lambda _: sys.exit()
     }
 }
 
